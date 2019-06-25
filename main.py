de841e315c8c56ce4e54da6309441f3ce3ca3604
和泉田済人 <sumito@izumita.com>
2019-06-25 12:23:34 +0900

    run関数を整備

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from discord.ext import commands
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 extensions = [
     'cogs.keeper'
 ]
 
 bot = commands.Bot("::")
 
+bot.run(os.environ.get("TOKEN"))
